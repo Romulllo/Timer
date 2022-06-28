be80a2f28c2c92341c49d60656574006c13b91a1
Romulo Santos <romulosantos88@hotmail.com>
2022-06-28 17:27:05 +0100

    start, pause and tick

--- index.js
+++ index.js
@@ -5,15 +5,37 @@ class Timer {
     this.pauseButton = pauseButton;
 
     this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
   }
 
-  start() {
-    console.log('Time to start the timer!');
-  }
+  start = () => {
+    this.tick();
+    this.intervalId = setInterval(this.tick, 1000);
+  };
+
+  pause = () => {
+    clearInterval(this.intervalId);
+  };
+
+  tick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+    } else {
+      this.timeRemaining = this.timeRemaining - 1;
+    }
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  };
+
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  };
 }
 
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton )
+const timer = new Timer(durationInput, startButton, pauseButton );
